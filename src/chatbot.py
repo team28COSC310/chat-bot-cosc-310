bafedb813d6c3a4e83c1dd11b7540366380aa9ec
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2022-03-04 20:42:05 -0800

    pylint cleanup

--- src/chatbot.py
+++ src/chatbot.py
@@ -8,24 +8,26 @@ Nicholas Brown, Jonathan Chou, Omar Ishtaiwi, Niklas Tecklenburg and Elizaveta Z
 import random
 import json
 import pickle
-import numpy as np
-
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-lemmatizer = WordNetLemmatizer()
-
 from response_model import ChatModel
 from prepare_training_data import build_training_data
-from data_importer import Intents, load_intents
+from data_importer import load_intents
+
+
+lemmatizer = WordNetLemmatizer()
 
 intents = load_intents("../intents.json")
 train_x, train_y = build_training_data(intents)
 
 chat_model = ChatModel(len(train_x[0]), len(train_y[0]))
 
-words = pickle.load(open("pickle/words.pkl", "rb"))
-classes = pickle.load(open("pickle/classes.pkl", "rb"))
+with(open("pickle/words.pkl", "rb")) as pickle_words:
+    words = pickle.load(pickle_words)
+
+with(open("pickle/classes.pkl", "rb")) as pickle_classes:
+    classes = pickle.load(pickle_classes)
 
 # Load the Chatbot model, if there are no weights available, train the model
 try:
@@ -35,9 +37,9 @@ except FileNotFoundError:
 
 
 def preprocess_sentence(sentence):
-    '''
+    """
     Tokenize the sentence entered by user into words, delete punctuation signs and lemmatize
-    '''
+    """
     ignore_letters = ['?', '!', '.', ',']
     sent_words = nltk.word_tokenize(sentence)
     sent_words = [lemmatizer.lemmatize(word) for word in sent_words if word not in ignore_letters]
@@ -45,10 +47,10 @@ def preprocess_sentence(sentence):
 
 
 def bag_words(sentence):
-    '''
+    """
     Create the bag of words representation of a sentence.
     That is, identify which words from our intents file are present in the users' sentence
-    '''
+    """
     sent_words = preprocess_sentence(sentence)
     # bag=np.zeros(len(intents.words))
     bag = [0] * len(words)
@@ -60,9 +62,9 @@ def bag_words(sentence):
 
 
 def predict_class(sentence):
-    '''
+    """
     Predict the class (intent) of a users' sentence
-    '''
+    """
     bow = bag_words(sentence)
     res = chat_model.predict(bow)[0]
     err_border = 0.3
@@ -76,9 +78,10 @@ def predict_class(sentence):
 
 
 def get_response(intents_list, intents_json):
-    '''
-    Generate a response of the bot, given the probable intents of a users and the list of all intents
-    '''
+    """
+    Generate a response of the bot,
+    given the probable intents of a users and the list of all intents
+    """
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
@@ -89,12 +92,13 @@ def get_response(intents_list, intents_json):
 
 
 if __name__ == '__main__':
-    intents = json.loads(open("../intents.json").read())
+    with(open("../intents.json")) as ints_json:
+        intents = json.loads(ints_json.read())
     print("You can start talking to the bot now. If you want to stop the bot, type `stop`")
     while True:
         message = input("")
         if message.lower() == 'stop':
             break
         ints = predict_class(message)
-        res = get_response(ints, intents)
-        print(res)
+        response = get_response(ints, intents)
+        print(response)
--- src/prepare_training_data.py
+++ src/prepare_training_data.py
@@ -6,7 +6,6 @@ import random
 import pickle
 import numpy as np
 
-import nltk
 from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
