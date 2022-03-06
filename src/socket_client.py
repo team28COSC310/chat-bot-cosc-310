89223b8f3b296f632873925991cd5bfa6c780a2c
Niklas Tecklenburg <2465258T@student.gla.ac.uk>
2022-03-06 01:02:22 -0800

    Chatbot can chat with itself via TCP Sockets running in different terminals/Machine

--- src/socket_client.py
+++ src/socket_client.py
@@ -1,15 +1,50 @@
 import socket
 import sys
+import pickle
+import json
+import time
+
+from chatbot import predict_class, get_response
+from response_model import ChatModel
+from prepare_training_data import build_training_data
+from data_importer import Intents, load_intents
+
+intents = load_intents("../intents.json")
+train_x, train_y = build_training_data(intents)
+
+chat_model = ChatModel(len(train_x[0]), len(train_y[0]))
+
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
+
+# Load the Chatbot model, if there are no weights available, train the model
+try:
+    chat_model.load_model_weights('./model_weights/weights.h5')
+except:
+    chat_model.train(train_x, train_y, './model_weights/weights.h5')
+
+intents = json.loads(open("../intents.json").read())
+
+print("Ready to Chat")
+
 
 #connect to server
 cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cli_sock.connect((sys.argv[1], int(sys.argv[2])))
 
+response = input("Conversation starter: ")
+cli_sock.sendall(response.encode('utf-8'))
+
 while True:
-    response = input(">>") 
-    cli_sock.sendall(response.encode('utf-8'))
     message = cli_sock.recv(1024).decode('utf-8')
-    print('Partner: ' + message)
+    print('ChatBot: ' + message)
+    
+    ints = predict_class(message)
+    response = get_response(ints, intents)
+    print("YouBot: " + response)
+    
+    cli_sock.sendall(response.encode('utf-8'))
+    time.sleep(4)
 
 print(out)
 cli_sock.close()
--- src/socket_server.py
+++ src/socket_server.py
@@ -1,5 +1,31 @@
 import socket
 import sys
+import pickle
+import json
+import time
+
+from chatbot import predict_class, get_response
+from response_model import ChatModel
+from prepare_training_data import build_training_data
+from data_importer import Intents, load_intents
+
+intents = load_intents("../intents.json")
+train_x, train_y = build_training_data(intents)
+
+chat_model = ChatModel(len(train_x[0]), len(train_y[0]))
+
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
+
+# Load the Chatbot model, if there are no weights available, train the model
+try:
+    chat_model.load_model_weights('./model_weights/weights.h5')
+except:
+    chat_model.train(train_x, train_y, './model_weights/weights.h5')
+    
+intents = json.loads(open("../intents.json").read())
+
+print("Ready to Chat")
 
 # Create socket
 srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,9 +40,14 @@ cli_sock, cli_addr = srv_sock.accept()
 
 while True:
     message = cli_sock.recv(1024).decode('utf-8')
-    print('Partner: ' + message)
-    response = input(">>") 
+    print('ChatBot: ' + message)
+    
+    ints = predict_class(message)
+    response = get_response(ints, intents)
+    print("YouBot: " + response)
+    
     cli_sock.sendall(response.encode('utf-8'))
+    time.sleep(4)
     
 print(out)
 cli_sock.close()
