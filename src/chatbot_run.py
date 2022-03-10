12743d4cae78f252a90cbb5c40cdcf8a7d173e36
ElizavetaZhukova1 <elizavetazhukova57@gmail.com>
2022-03-09 17:47:33 -0800

    Created file to run chatbot. Updated Readme. Deleted an ability to run gui from gui file itself

---
+++ src/chatbot_run.py
@@ -0,0 +1,10 @@
+from app_gui import ChatApplication
+"""
+UBCO Chatbot. Run this file to run the Chatbot with gui
+
+Created by:
+Nicholas Brown, Jonathan Chou, Omar Ishtaiwi, Niklas Tecklenburg and Elizaveta Zhukova
+
+"""
+app_gui = ChatApplication()
+app_gui.run()
